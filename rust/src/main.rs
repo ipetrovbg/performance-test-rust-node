91a18bfe246007ed93545f19f8e4bed3da238647
petarpetrov <ipetrovbg@gmail.com>
2022-09-11 13:26:32 +0300

    add rust

---
+++ rust/src/main.rs
@@ -0,0 +1,24 @@
+use lambda_runtime::{handler_fn};
+use log::{info, LevelFilter};
+use simple_logger::SimpleLogger;
+
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), lambda_runtime::Error> {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
+
+    let handler_callback = handler_fn(rust_handler);
+    lambda_runtime::run(handler_callback).await?;
+
+    Ok(())
+}
+
+async fn rust_handler() -> Result<(), Error> {
+    info!("Initializing rust lambda!");
+
+    Ok(())
+}
